105a411ea858397432d61adc940693a46b1d4b6c
syed SaifD <sydsaifullah7@gmail.com>
2020-08-30 02:31:30 +0530

    Created login/sign up page

---
+++ todolist/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('sign_up', views.sign_up, name = 'sign_up')
+]
--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def index(req):
+    d = {'title':'my title'}
+    return render(req, 'todolist/home.html', context = d)
+
+def login(req):
+    ctx = {'title':'To-Do Listy-Login/Sign-up',
+            'login':True
+    }
+    return render(req, 'todolist/login.html', context = ctx)
+
+def sign_up(req):
+    ctx = {'title':'To-Do Listy-Login/Sign-up',
+            'login':False
+    }
+    return render(req, 'todolist/login.html', context = ctx)
