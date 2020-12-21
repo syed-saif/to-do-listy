eaa532be31210f7c5015fd8bf1cdfd5f701f01f8
Syed Saif <sydsaifullah7@gmail.com>
2020-12-21 17:32:58 +0530

    settings fix

--- todo/settings.py
+++ todo/settings.py
@@ -121,6 +121,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'stat'),
+)
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'home'
